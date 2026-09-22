33a20b9e3db617d111dbc283c3df7854a32ac7e4
canchous <novikovsasha40@gmail.com>
2026-09-22 20:52:42 +0300

    fix(client): read full request instead of 1024 bytes

--- client.py
+++ client.py
@@ -6,6 +6,8 @@ client_socket.connect(("127.0.0.1", 1159))
 
 client_socket.sendall(b"Hello ")
 
-
-data = client_socket.recv(1024)
-print(data.decode())
+while True:
+    data = client_socket.recv(1024)
+    if len(data) == 0:
+        break
+    print(data.decode())
